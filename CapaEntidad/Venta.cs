1d9624fe1464d2ba6e30af71dcb0a01ad3abdd37
Dayana Vega <Dayana Vega@DESKTOP-HV3FS3A>
2024-05-19 15:20:06 -0600

    Agregar archivos de proyecto.

---
+++ CapaEntidad/Venta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    /*
+     * 
+     * IdVenta int primary key identity,
+IdCliente int references CLIENTE(IdCliente),
+TotalProducto int,
+MontoTotal decimal(10,2),
+Contacto varchar(50),
+IdDepto varchar(10),
+Telefono varchar(50),
+Direccion varchar(500),
+IdTransaccion varchar(50),
+    */
+
+    public class Venta
+    {
+        public int IdVenta { get; set; }
+        public int IdCliente { get; set; }
+        public int TotalProducto { get; set; }
+        public decimal MontoTotal { get; set; }
+        public string Contacto { get; set; }
+        public string IdDepto { get; set; } // IdDistrito
+        public string Telefono { get; set; }
+        public string Direccion { get; set; } //FechaText
+        public string IdTransaccion { get; set; }
+    }
+}
